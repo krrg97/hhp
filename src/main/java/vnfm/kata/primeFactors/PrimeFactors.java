81c5a0f81d0c2e04ef122a9be1d2611de7532622
krrg97 <rodion.krivoshein@gmail.com>
2016-06-12 23:04:52 +0400

    Add prime factor for two

--- src/main/java/vnfm/kata/primeFactors/PrimeFactors.java
+++ src/main/java/vnfm/kata/primeFactors/PrimeFactors.java
@@ -1,11 +1,20 @@
 package vnfm.kata.primeFactors;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface PrimeFactors
 {
     default Stream<Integer> generate(int number)
     {
-        return Stream.empty();
+        List<Integer> primes = new ArrayList<>();
+        if (number > 1)
+        {
+            primes.add(2);
+        }
+
+        return primes.stream();
     }
+
 }
